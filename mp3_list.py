feba3c772faeb8b530940973d571250cc7508316
adaeo <wangj9507@gmail.com>
2021-11-22 21:04:00 +1000

    Additional comments added

--- mp3_list.py
+++ mp3_list.py
@@ -9,6 +9,7 @@ from utility import optdict, audio_ext, sepdict
 
 ## Define Classes ##
 
+# DTF Object type
 class ListFile:
     def __init__(self, name, ext):
         self.name = name
@@ -17,7 +18,7 @@ class ListFile:
 
 ## Define Functions ##
 
-
+# Function run when program is called through terminal
 def initial():
     arg_length = len(sys.argv)
 
@@ -28,30 +29,42 @@ def initial():
     # Normal Usage
     elif arg_length >= 3:
 
+        # Parse optional flags
         if arg_length > 3:
             optargs = sys.argv[3:arg_length]
-            
+            # Run optional flags
             for arg in optargs:
-                optdict[arg]()
+                try:
+                    # Run optional flag if it exists in optdict
+                    optdict[arg]()
+                except KeyError:
+                    # Stop program if flag does not exist
+                    error_msg = "Invalid flag \"{0}\"".format(arg)
+                    raise SystemExit(error_msg)
     
-
+        # Set DTF and DIR arguments. 
         dtf_arg = str(sys.argv[1])
         dir_arg = str(sys.argv[2])
 
         try:
+            # Try split DTF argument string
             split_dtf =  dtf_arg.split(".")
             dtf = ListFile(split_dtf[0], split_dtf[1])
         except IndexError:
+            # Stop program if argument is invalid
             error_msg = "Invalid file name \"{0}\"".format(dtf_arg)
             raise SystemExit(error_msg)
 
         try:
+            # Try load DTF into Pandas Dataframe
             dtf_df = pd.read_csv(dtf_arg, header=None, sep=sepdict[dtf.ext], skiprows=s.skip)
         except FileNotFoundError:
+            # Stop program if file does not exist
             error_msg = "File \"{0}\" does not exist!".format(dtf_arg)
             raise SystemExit(error_msg)
         
         try: 
+            # Try locate Directory 
             os.chdir(dir_arg)
             audio_files = []
             for files in audio_ext:
@@ -62,11 +75,13 @@ def initial():
             else:
                 error_msg = "No suitable audio files found in directory."
                 raise SystemExit(error_msg)
+                # Stop if directory is invalid
         except FileNotFoundError:
             error_msg = "Directory \"{0}\" does not exist!".format(dir_arg)
             raise SystemExit(error_msg)
 
     else:
+        # Stop program if args are not at least 2
         print("\nAt least two arguments required!")
         print("Required:\n- Delimeted-Text File: \"PATH\\file.ext\"")
         print("- Audio Directory: \"PATH\\dir\"\n")
